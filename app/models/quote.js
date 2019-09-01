e59cc8ca7cf5ca769e1a66bd76d34a0e1c984f65
Rolland <Rollandcampbell@msn.com>
2019-09-01 12:05:13 -0600

    moving stuff on page

--- app/models/quote.js
+++ app/models/quote.js
@@ -6,7 +6,7 @@ export default class Quote {
 
   get Template() {
     return `
-  <div class="card" style = "width: 40rem;" >
+      <div class="card" style = "width: 18rem;" >
         <div class="card-body bg-dark text-white">
           <h5 class="card-title">Quote of the day</h5>
           <p class="card-text">${this.quote}<br> ~${this.author}<br>
